453a5b68b47748cec92f5ec0b9e467ac56ce1edb
David Bérichon <david.berichon@ercom.fr>
2017-02-03 11:19:21 +0100

    forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/david-berichon/clirescue/cmdutil"
+	"github.com/david-berichon/clirescue/user"
 )
 
 var (
